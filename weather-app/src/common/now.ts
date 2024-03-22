59451c7c48bb0db71a0ea3f87625e7d67aa19b5e
jewelcodes <omarmelghoul01@gmail.com>
2024-03-22 13:29:04 -0400

    pull current conditions from openweathermap

---
+++ weather-app/src/common/now.ts
@@ -0,0 +1,37 @@
+
+async function rawWeather(place:string, units:string) {
+    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?appid=7b38e8c5a03a1636803ff7e9127b7b69&units=" + units + "&q=" + place);
+    if(response.status != 200 || !response.ok) {
+        return null;
+    }
+
+    let data = await response.json();
+    return data;
+}
+
+/*
+ * now(): returns the weather conditions for a given place
+ * Parameter: place - city or zip code
+ * Returns: JSON object
+ */
+
+export async function now(place:string, units:string) {
+    let object:any = {};
+    let raw:any = rawWeather(place, units);
+
+    if(raw == null) return null;
+
+    object.place = raw.name;
+    object.condition = raw.weather.main;
+    object.time = raw.dt;
+    object.timezone = raw.timezone;
+    object.temp = raw.main.temp;
+    object.feels = raw.main.feels_like;
+    object.low = raw.main.temp_min;
+    object.high = raw.main.temp_max;
+    object.pressure = raw.main.pressure;
+    object.wind = raw.wind.speed;
+    object.gust = raw.wind.gust;
+
+    return object;
+}
