05c50a96c39dd96c4cebc713a4fcfdb436752f0d
jewelcodes <omarmelghoul01@gmail.com>
2024-03-24 12:28:48 -0400

    icons for different weather conditions

--- weather-app/src/components/DailyForecast/DailyForecast.tsx
+++ weather-app/src/components/DailyForecast/DailyForecast.tsx
@@ -1,11 +1,12 @@
 import React, {useContext, useEffect, useState} from 'react';
 import './DailyForecast.css';
-import { isLoading, currentLocation, getForecast } from '../../common/locations';
+import { isLoading, currentLocation, getForecast, getConditions } from '../../common/locations';
 
 function DailyForecast(props:any) {
     const dailyEntry = (d:number, min:number, max:number) => {
         let forecast:any = getForecast(currentLocation());
-        let day:any;
+        let conditions:any = getConditions(currentLocation());
+        let now = Date.now()/1000;
         const days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
 
         if(isLoading() || forecast == null) {
@@ -17,13 +18,38 @@ function DailyForecast(props:any) {
                 </div>
             );
         } else {
-            day = new Date(forecast.forecast[d].timestamp);
+            let day:any = new Date(forecast.forecast[d].timestamp);
             day = day.getUTCDay();
+
+            let icon:any;
+            let isNight = (now < conditions.sunrise || now > conditions.sunset);
+            let conditionText:any;
+
+            if(d == 0) {
+                conditionText = conditions.condition;
+            } else {
+                conditionText = forecast.forecast[d].condition;
+            }
+            
+            // for current day only
+            if(conditionText == "Cloudy") {
+                icon = "fa-solid fa-cloud";
+            } else if(conditionText == "Rain") {
+                icon = "fa-solid fa-cloud-rain"
+            } else if(conditionText == "Snow") {
+                icon = "fa-solid fa-snowflake";
+            } else if(conditionText == "Mist" || conditionText == "Fog" || conditionText == "Haze") {
+                icon = "fa-solid fa-smog";
+            } else if(d == 0 && isNight) {
+                icon = "fa-solid fa-cloud-moon";
+            } else {
+                icon = "fa-solid fa-sun";
+            }
 
             return (
                 <div className="dailyEntry">
                     <div className="day"><strong>{d == 0 ? "Today" : days[day]}</strong></div>
-                    <div className="icon"></div>
+                    <div className="icon"><i className={icon} title={conditionText}></i></div>
                     <div className="temp"></div>
                 </div>
             );
