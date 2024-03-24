f05112bbaaef2210c4524b44e68c89bc4331d6b0
jewelcodes <omarmelghoul01@gmail.com>
2024-03-24 11:57:21 -0400

    more work on daily forecast table

--- weather-app/src/components/DailyForecast/DailyForecast.tsx
+++ weather-app/src/components/DailyForecast/DailyForecast.tsx
@@ -1,12 +1,43 @@
 import React, {useContext, useEffect, useState} from 'react';
 import './DailyForecast.css';
-import { currentLocation } from '../../common/locations';
+import { isLoading, currentLocation, getForecast } from '../../common/locations';
 
 function DailyForecast(props:any) {
+    const dailyEntry = (d:number, min:number, max:number) => {
+        let forecast:any = getForecast(currentLocation());
+        let day:any;
+        const days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+
+        if(isLoading() || forecast == null) {
+            return (
+                <div className="dailyEntry">
+                    <div className="day"><strong>--</strong></div>
+                    <div className="icon"></div>
+                    <div className="temp"></div>
+                </div>
+            );
+        } else {
+            day = new Date(forecast.forecast[d].timestamp + " 00:00:00 UTC");
+            day = day.getUTCDay();
+
+            return (
+                <div className="dailyEntry">
+                    <div className="day"><strong>{d == 0 ? "Today" : days[day]}</strong></div>
+                    <div className="icon"></div>
+                    <div className="temp"></div>
+                </div>
+            );
+        }
+    };
 
     return (
-        <div class="daily">
+        <div className="daily">
             <h1>DAILY FORECAST</h1>
+            {dailyEntry(0, 0, 0)}
+            {dailyEntry(1, 0, 0)}
+            {dailyEntry(2, 0, 0)}
+            {dailyEntry(3, 0, 0)}
+            {dailyEntry(4, 0, 0)}
         </div>
     );
 }
