785d5fadcf186342f9f7dcfd2cd07d6b32f6f5a8
jewelcodes <omarmelghoul01@gmail.com>
2024-03-24 22:15:24 -0400

    start of search component

---
+++ weather-app/src/components/Search/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Search.css';
+
+function Search(props:any) {
+    return (
+        <div className="search">
+            <input type="text" />
+        </div>
+    );
+}
+
+export default Search;
